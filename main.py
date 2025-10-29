1b79d09b2bcb59e17f3835fde029e9caaf157fa3
karu <karu@pop-os.localdomain>
2025-10-29 20:09:29 +0100

    New actions

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 #Un número perfecto es un número entero positivo que es igual a la suma de sus divisores.
+
 def is_perfect(n):
+    ### ELIMINAR PARA QUE LOS ALUMNOS RESUELVAN
     if n <= 0:
         raise ValueError("El número debe ser positivo")
-    
-    suma = sum(i for i in range(1, n) if n % i == 0)
-    return suma == n
+    suM = sum(i for i in range(1, n) if n % i == 0)
+    return suM == n
 
